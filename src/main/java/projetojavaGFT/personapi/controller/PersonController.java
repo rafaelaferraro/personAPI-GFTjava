e73004a57f0b5c032a13bdbb8d8aa9fe2d088492
Rafaela de Souza Dantas Ferraro <rafaelasdferraro@gmail.com>
2021-07-24 09:22:40 -0300

    Develop delete by id operation

--- src/main/java/projetojavaGFT/personapi/controller/PersonController.java
+++ src/main/java/projetojavaGFT/personapi/controller/PersonController.java
@@ -39,4 +39,11 @@ public class PersonController {
         return personService.findById(id);
     }
 
+    @DeleteMapping ("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void  deleteById(@PathVariable Long id) throws PersonNotFoundException {
+        personService.delete(id);
+
+
+    }
 }
